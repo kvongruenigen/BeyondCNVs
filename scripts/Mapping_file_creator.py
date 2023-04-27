0cddb7c9d8dff81ede5e344cf2d7e60ae9f5ade1
kvongruenigen <kay.vongruenigen@uzh.ch>
2023-04-27 15:35:46 +0200

    Script updates

--- scripts/Mapping_file_creator.py
+++ scripts/Mapping_file_creator.py
@@ -17,7 +17,7 @@ import os
 relevant = ['Tumor_Sample_UUID','Matched_Norm_Sample_UUID','case_id', 'NCBI_Build','Chromosome',
           'Start_Position','End_Position','Strand', 'Variant_Classification', 'Variant_Type',
           'Reference_Allele', 'Tumor_Seq_Allele1', 'Tumor_Seq_Allele2', 'HGVSc', 'HGVSp',
-          'HGVSp_Short', 'all_effects'] # combined identifiers and variant information
+          'HGVSp_Short'] # combined identifiers and variant information
 
 imf = pd.DataFrame() # Create empty dataframe for intermediate file
 
@@ -26,7 +26,7 @@ for info in relevant: # Assign columns from relevant list
 
 for file in os.listdir("data/"): # Iterate through directory
     if '.maf' in str(file): # discard MANIFEST.txt and other non-maf files
-        df = pd.read_csv("data/"+ str(file), sep='\t', skiprows=7, header=0) # read in the maf file as df
+        df = pd.read_csv("data/"+ str(file), sep='\t', skiprows=7, header=0, low_memory = False) # read in the maf file as df
         df = df[relevant] # subset dataframe to relevant information
         imf = pd.concat([imf, df]) # write the relevant information to imf
 
