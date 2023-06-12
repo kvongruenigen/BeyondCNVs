e723820fa8b2b83d602270f1cdcf49bdf820f37f
kvongruenigen <kay.vongruenigen@uzh.ch>
2023-06-13 00:02:39 +0200

    Fixed maf download

--- dev/test.py
+++ dev/test.py
@@ -60,8 +60,8 @@ print('Found ', len(file_uuid_list), 'files to download.')
 
 # Import record of existing files
 existing_ids = []
-if os.path.isfile('data/existing_file_ids.txt'):
-    with open('data/existing_file_ids.txt') as f:
+if os.path.isfile('../data/existing_file_ids.txt'):
+    with open('../data/existing_file_ids.txt') as f:
         lines = f.readlines()
     for i in lines:
         existing_ids.append(i)
@@ -82,48 +82,11 @@ ls = []
 for i in range(0, len(file_uuid_list), 1000):
     ls.append(file_uuid_list[i:i + 1000])
 
-# Download the files
-print('Starting', len(ls),'downloads...')
-downloaded = 1
-for idls in ls:
-
-    # Exclude existing files
-    for ids in idls:
-        if ids in existing_ids:
-            idls.remove(ids)
-            print('Removed existing file id:', ids)
-    
-    data_endpt = "https://api.gdc.cancer.gov/data"
-
-    params = {"ids": idls}
-
-    response = requests.post(data_endpt,
-                            data=json.dumps(params),
-                            headers={"Content-Type": "application/json"})
-
-    response_head_cd = response.headers["Content-Disposition"]
-
-    file_name = re.findall("filename=(.+)", response_head_cd)[0]
-
-     # Check for the directory
-    os.makedirs('temp/', exist_ok=True)
-    save_path = 'temp/'
-
-    completeName = os.path.join(save_path, file_name)
-
-    with open(completeName, "wb") as output_file:
-        output_file.write(response.content)
-
-    if len(ls) - downloaded != 0:
-        print('Downloaded completed. Remaining:', len(ls) - downloaded, 'of', len(ls))
-        downloaded += 1
-    else:
-        print('Download finished')
 
 # Create record if imported file UUIDs
 for i in file_uuid_list:
     i = i + '\n'
     existing_ids.append(i)
-with open('data/existing_file_ids.txt', 'w') as file_ids:
+with open('../data/existing_file_ids.txt', 'w') as file_ids:
     for i in existing_ids:
         file_ids.write(i)
